84c76b161c6cb7010f658feb03aa64c90d59cb51
Hassan Ngoma <munyiseke@gmail.com>
2025-08-20 19:49:20 +0300

    Modified statement

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,6 +2,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment.prod';
 
 interface LoginRequest {
   email: string;
@@ -22,19 +23,20 @@ interface ChangePasswordRequest {
   providedIn: 'root'
 })
 export class AuthService {
-  private baseUrl = 'http://localhost:7080/api/auth';
+  // private baseUrl = 'http://localhost:7080/api/auth';
+  private baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
   login(request: LoginRequest): Observable<LoginResponse> {
-    return this.http.post<LoginResponse>(`${this.baseUrl}/login`, request);
+    return this.http.post<LoginResponse>(`${this.baseUrl + "/api/auth"}/login`, request);
   }
 
   changePassword(data: {
     currentPassword: string;
     newPassword: string;
   }): Observable<any> {
-    return this.http.post(`${this.baseUrl}/change-password`, data);
+    return this.http.post(`${this.baseUrl + "/api/auth"}/change-password`, data);
   }
 
   saveToken(token: string) {
